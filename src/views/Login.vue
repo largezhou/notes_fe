02ce783cafa51a45f9f7add3f31ae28fcfa4260f
largezhou <largezhou@163.com>
2018-09-01 17:10:20 +0800

    登录表单

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,24 @@
 <template>
   <div style="text-align: center">
     <h1>登录</h1>
-    <el-button @click="onClick">点击登录</el-button>
+
+    <el-form
+      label-width="80px"
+      :model="loginForm"
+      ref="loginForm"
+      :rules="loginRules"
+      style="margin: auto; width: 300px;"
+    >
+      <el-form-item label="手机号码" prop="mobile">
+        <el-input v-model="loginForm.mobile"/>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="loginForm.password" type="password"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="onClick">点击登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -10,19 +27,36 @@ import { Message } from 'element-ui'
 
 export default {
   name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        mobile: '',
+        password: '',
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '必须填写手机号码', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: '必须填写密码', trigger: 'blur' },
+        ],
+      },
+    }
+  },
   methods: {
     onClick() {
-      this.$store
-        .dispatch('login', {
-          data: {
-            mobile: '18688472412',
-            password: '000000',
-          },
-        })
-        .then(res => {
-          Message.success('登录成功')
-          this.$router.push({ name: 'needAuth' })
-        })
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.$store
+            .dispatch('login', {
+              data: this.loginForm,
+            })
+            .then(res => {
+              Message.success('登录成功')
+              this.$router.push({ name: 'needAuth' })
+            })
+        }
+      })
     },
   },
 }
