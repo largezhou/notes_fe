c74b8769ae9b59d5975f453995c10e643a8c57de
largezhou <largezhou@163.com>
2018-09-01 09:46:38 +0800

    调整eslint配置|安装elementui和axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button @click="onClick">ELButton</el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { Message } from 'element-ui'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  methods: {
+    onClick() {
+      Message('呵呵')
+    },
+  },
 }
 </script>
