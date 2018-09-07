709bab44bddcb8620ba910b1714f724ab0facf04
largezhou <largezhou@163.com>
2018-09-07 23:22:01 +0800

    导航栏右侧加入一个更多操作的下拉列表

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,13 @@
             <router-link to="/login">登录</router-link>
             <router-link to="/need-auth">需要登录</router-link>
             <router-link to="/404">404</router-link>
+            <el-dropdown class="more pull-right" trigger="click">
+              <a href="javascript:void(0);" class="el-icon-more"></a>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>编辑模式</el-dropdown-item>
+                <el-dropdown-item>退出登录</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
           </el-col>
         </el-row>
       </div>
