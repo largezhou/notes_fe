4a07f75aef472410168e08b31e1c270d62ce5a65
largezhou <largezhou@163.com>
2018-09-13 08:20:05 +0800

    笔记列表独立成组件

---
+++ src/components/BookNoteItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-card>
+    <div slot="header">
+      <div class="time">
+        <human-time :time="item.created_at" prefix="发布于："/>
+      </div>
+      <div>
+        <div v-if="!disableBelong" class="belong">
+          <div v-if="item.book">
+            <router-link :to="`/books/${item.book.id}`">{{ item.book.title }}</router-link>
+            •
+            <router-link :to="`/books/${item.book.id}/pages/${item.page}`">第{{ item.page }}页</router-link>
+          </div>
+          <div v-else>书不见啦~</div>
+        </div>
+        <div v-if="item.title" class="title">
+          <router-link :to="`/notes/${item.id}`">{{ item.title }}</router-link>
+        </div>
+      </div>
+    </div>
+    <div>
+      <router-link :to="`/notes/${item.id}`">{{ item.desc }}</router-link>
+    </div>
+    <div class="card-footer">
+      <el-tag
+        v-for="tag of item.tags"
+        :key="tag.id"
+        size="small"
+        disable-transitions
+      >
+        <router-link :to="`/tags/${tag.id}`">{{ tag.name }}</router-link>
+      </el-tag>
+    </div>
+  </el-card>
+</template>
+
+<script>
+import HumanTime from '@/components/HumanTime'
+
+export default {
+  name: 'BookNoteItem',
+  components: { HumanTime },
+  props: {
+    item: Object,
+    disableBelong: Boolean,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -5,42 +5,11 @@
         <book-widget/>
       </el-col>
       <el-col :md="18" :sm="20" class="notes-container">
-        <el-card
+        <book-note-item
           v-for="item of notes"
           :key="item.id"
-        >
-          <div slot="header">
-            <div class="time">
-              <human-time :time="item.created_at" prefix="发布于："/>
-            </div>
-            <div>
-              <div class="belong">
-                <div v-if="item.book">
-                  <router-link :to="`/books/${item.book.id}`">{{ item.book.title }}</router-link>
-                  •
-                  <router-link :to="`/books/${item.book.id}/pages/${item.page}`">第{{ item.page }}页</router-link>
-                </div>
-                <div v-else>书不见啦~</div>
-              </div>
-              <div v-if="item.title" class="title">
-                <router-link :to="`/notes/${item.id}`">{{ item.title }}</router-link>
-              </div>
-            </div>
-          </div>
-          <div>
-            <router-link :to="`/notes/${item.id}`">{{ item.desc }}</router-link>
-          </div>
-          <div class="card-footer">
-            <el-tag
-              v-for="tag of item.tags"
-              :key="tag.id"
-              size="small"
-              disable-transitions
-            >
-              <router-link :to="`/tags/${tag.id}`">{{ tag.name }}</router-link>
-            </el-tag>
-          </div>
-        </el-card>
+          :item="item"
+        />
       </el-col>
     </el-row>
   </div>
@@ -49,11 +18,11 @@
 <script>
 import { getNotes } from '../api/notes'
 import BookWidget from '@/components/BookWidget'
-import HumanTime from '@/components/HumanTime'
+import BookNoteItem from '@/components/BookNoteItem'
 
 export default {
   name: 'Index',
-  components: { BookWidget, HumanTime },
+  components: { BookWidget, BookNoteItem },
   data() {
     return {
       notes: [],
