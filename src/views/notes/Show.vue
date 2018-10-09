4f7c5c83845551ceacf6cb36dc7cebb9e8bf85c4
largezhou <largezhou@163.com>
2018-10-09 21:33:33 +0800

    变化时，如果这是hash变了，则不重新获取数据

--- src/views/notes/Show.vue
+++ src/views/notes/Show.vue
@@ -21,12 +21,14 @@
 import { getNote } from '@/api/notes'
 import BookInfoCard from '@/components/BookInfoCard'
 import Tag from '@/components/Tag'
+import _ from 'lodash'
 
 export default {
   name: 'Show',
   components: { BookInfoCard, Tag },
   data: () => ({
     note: null,
+    oldRoute: null,
   }),
   methods: {
     getData(id = this.$route.params.noteId) {
@@ -56,11 +58,25 @@ export default {
         ft.parentElement.insertBefore(anchor, ft)
       }
     },
+
+    // 如果路径相同，query 也相同，则表示路由变化时，只有 hash 变了
+    checkOnlyHashChange() {
+      return this.oldRoute &&
+        this.oldRoute.path === this.$route.path &&
+        _.isEqual(this.oldRoute.query, this.$route.query)
+    },
   },
   watch: {
     $route: {
       handler(newValue) {
-        this.getData()
+        // 当不是只有 hash 变了时，更新数据，并记录旧的 path 和 query
+        if (!this.checkOnlyHashChange()) {
+          this.oldRoute = {
+            path: this.$route.path,
+            query: this.$route.query,
+          }
+          this.getData()
+        }
       },
       immediate: true,
     },
