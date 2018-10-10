ec9371b340e8960f2a3961d1e3ad6252d64d7ae1
largezhou <largezhou@163.com>
2018-10-10 08:19:24 +0800

    登录表单恢复

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,68 +1,112 @@
 <template>
-  <div style="text-align: center">
-    <h1>登录</h1>
-
-    <el-form
-      label-width="80px"
-      :model="loginForm"
-      ref="loginForm"
-      :rules="loginRules"
-      style="margin: auto; width: 300px;"
-    >
-      <el-form-item label="手机号码" prop="mobile">
-        <el-input v-model="loginForm.mobile"/>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input v-model="loginForm.password" type="password"/>
-      </el-form-item>
-      <el-form-item>
-        <el-button @click="onClick">点击登录</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+  <page-layout>
+    <v-form ref="form" class="login-form">
+      <p class="title">登录吧</p>
+      <v-text-field
+        label="用户名"
+        :error-messages="validateErrors('form.username')"
+        v-model="$v.form.username.$model"
+      />
+      <v-text-field
+        label="密码"
+        type="password"
+        :error-messages="validateErrors('form.password')"
+        v-model="$v.form.password.$model"
+      />
+      <v-btn color="primary" @click="onLogin">登录</v-btn>
+    </v-form>
+  </page-layout>
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, minLength, maxLength } from 'vuelidate/lib/validators'
+import _ from 'lodash'
+
 export default {
   name: 'Login',
+  mixins: [validationMixin],
+  validations: {
+    form: {
+      username: {
+        required,
+        minLength: minLength(6),
+        maxLength: maxLength(20),
+      },
+      password: {
+        required,
+        minLength: minLength(6),
+        maxLength: maxLength(20),
+      },
+    },
+  },
   data() {
     return {
-      loginForm: {
-        mobile: '',
+      form: {
+        username: '',
         password: '',
       },
-      loginRules: {
-        mobile: [
-          { required: true, message: '必须填写手机号码', trigger: 'blur' },
-        ],
-        password: [
-          { required: true, message: '必须填写密码', trigger: 'blur' },
-        ],
+
+      attrs: {
+        form: {
+          username: {
+            required: '用户名要填的',
+            minLength: '用户名只能这么长：6 - 20',
+            maxLength: '用户名只能这么长：6 - 20',
+          },
+          password: {
+            required: '密码也是要填的',
+            minLength: '用户名只能这么长：6 - 20',
+            maxLength: '用户名只能这么长：6 - 20',
+          },
+        },
       },
     }
   },
   methods: {
-    onClick() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.$store
-            .dispatch('login', {
-              data: this.loginForm,
-            })
-            .then(res => {
-              this.$snackbar('登录成功')
-              const r = this.$route.query._redirect
-              const to = r || { name: 'index' }
+    validateErrors(key) {
+      const data = _.get(this.$v, key)
+      // 输入框没有输入过值时，不要显示错误消息
+      if (!data.$dirty) {
+        return
+      }
 
-              this.$router.push(to)
-            })
+      const validators = data.$params
+      for (const vt of Object.keys(validators)) {
+        if (!data[vt]) {
+          return _.get(this.attrs, key)[vt]
         }
-      })
+      }
+    },
+
+    onLogin() {
+      this.$v.$touch()
+
+      if (this.$v.$invalid) {
+        return
+      }
+
+      this.$store
+        .dispatch('login', {
+          data: this.form,
+        })
+        .then(res => {
+          this.$snackbar('登录成功')
+          const r = this.$route.query._redirect
+          const to = r || { name: 'index' }
+
+          this.$router.push(to)
+        })
     },
   },
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.login-form {
+  text-align: center;
+  width: 300px;
+  margin: auto;
+  margin-top: 100px;
+}
 </style>
