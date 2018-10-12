08188dff5810f317a107e1be400c04e31d09cf8a
largezhou <largezhou@163.com>
2018-10-12 20:36:00 +0800

    标签mock调整，支持热门，搜索，和所有

--- src/mocks/modules/tags.js
+++ src/mocks/modules/tags.js
@@ -4,8 +4,7 @@ import _ from 'lodash'
 
 // 获取所有标签，可搜索
 {
-  const key = 'tags|20'
-  const searchKey = 'tags|1-3'
+  let key = 'tags|20'
   // 保持模板为同一个对象,这样id才会持续增加
   const tagTmpl = {
     'id|+1': 1,
@@ -19,28 +18,38 @@ import _ from 'lodash'
 
   mock(/\/tags(\?.*)?/, 'get', template, (tmpl, options) => {
     const q = utils.queryFromUrl(options.url)
-    // 模拟搜索，只能搜到1-3个，并且有几率搜不到
     if (typeof q.q == 'string') {
+      // 模拟搜索，只能搜到1-3个，并且有几率搜不到
+
       // 如果搜到了，先去掉默认的20个的模板，之后如果不是搜索，则恢复
       if (Math.random() > 0.7) {
         delete tmpl[key]
+        key = 'tags|1-3'
 
         tagTmpl.name = _.trimStart(q.q) + '@cword'
 
-        tmpl[searchKey] = [
+        tmpl[key] = [
           tagTmpl,
         ]
       } else {
         tmpl[key] = []
       }
-    } else {
+    } else if (q.scope == 'hot') {
+      // 热门的10个
+      delete tmpl[key]
+      delete tagTmpl.name
+
+      key = 'tags|10'
       tagTmpl['name|2-5'] = '@cword'
+      tmpl[key] = [tagTmpl]
+    } else {
+      // 默认的所有key，50到100个
+      delete tmpl[key]
+      delete tagTmpl.name
 
-      tmpl[key] = [
-        tagTmpl,
-      ]
-
-      delete tmpl[searchKey]
+      key = 'tags|50-100'
+      tagTmpl['name|2-5'] = '@cword'
+      tmpl[key] = [tagTmpl]
     }
   })
 }
