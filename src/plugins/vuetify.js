6a08031e8d3d311e75a8cbfda068e1bc91579424
largezhou <largezhou@163.com>
2018-10-17 16:12:49 +0800

    vuetifyjs引入时，指定mdi为图标集

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,7 @@ import 'vuetify/dist/vuetify.min.css'
 import zhHans from 'vuetify/es5/locale/zh-Hans'
 
 Vue.use(Vuetify, {
+  iconfont: 'mdi',
   lang: {
     locales: { zhHans },
     current: 'zhHans',
