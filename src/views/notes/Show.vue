8ee6297bc599f088e9c755d7d0e748a92a50834c
largezhou <largezhou@163.com>
2018-11-20 13:52:31 +0800

    去掉页面标题修改

--- src/views/notes/Show.vue
+++ src/views/notes/Show.vue
@@ -11,36 +11,17 @@
 <script>
 import { getNote } from '@/api/notes'
 import BookInfoCard from '@/components/BookInfoCard'
-import _ from 'lodash'
-import seo from '@/mixins/seo'
 import ignoreHashChange from '@/mixins/ignore_hash_change'
 import PostDetailCard from '@/components/PostDetailCard'
 
 export default {
   name: 'Show',
   components: { BookInfoCard, PostDetailCard },
-  mixins: [seo, ignoreHashChange],
+  mixins: [ignoreHashChange],
   data: () => ({
     note: null,
     book: null,
   }),
-  computed: {
-    pageTitle() {
-      if (this.note == null) {
-        return ''
-      }
-
-      return `${_.get(this.note, 'book.title')} 第${this.note.page}页 ${_.get(this.note, 'title') || ''}`
-    },
-    pageDesc() {
-      return this.note ? this.note.desc : ''
-    },
-    pageKeywords() {
-      const tags = this.note ? this.note.tags : []
-
-      return tags.map(t => t.name).join(',')
-    },
-  },
   methods: {
     getData(id = this.$route.params.noteId) {
       getNote(id)
