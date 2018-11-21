db0d86a9bfa27ab0ae3d4e3d1e00c447f14eca0b
largezhou <largezhou@163.com>
2018-11-21 08:01:45 +0800

    简单分页

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -11,6 +11,15 @@
           :item="item"
           @force-deleted="onForceDelete(item, index)"
         />
+
+        <div class="text-xs-center" v-if="page">
+          <v-pagination
+            v-model="currentPage"
+            :length="totalPage"
+            :total-visible="totalVisible"
+            @input="onPageChange"
+          />
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
@@ -21,6 +30,7 @@ import { getNotes } from '../api/notes'
 import BookWidget from '@/components/BookWidget'
 import BookNoteItem from '@/components/BookNoteItem'
 import reloadData from '@/mixins/reload_data'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Index',
@@ -30,7 +40,15 @@ export default {
     return {
       notes: [],
       page: null,
+      totalVisible: 9,
+      currentPage: 1,
+      totalPage: 1,
     }
+  },
+  computed: {
+    ...mapState({
+      widescreen: state => state.app.widescreen,
+    }),
   },
   methods: {
     getData() {
@@ -39,14 +57,34 @@ export default {
           const data = res.data
           this.notes = data.data
           this.page = data.meta
+
+          this.currentPage = this.page.current_page
+          this.totalPage = this.page.last_page
         })
     },
     onForceDelete(item, index) {
       this.notes.splice(index, 1)
     },
+
+    // 如果侦听 currentPage 属性，则又会有页面缓存了，导致多次获取数据的问题
+    onPageChange(page) {
+      this.$router.push({
+        name: this.$route.name,
+        query: Object.assign({}, this.$route.query, { page }),
+      })
+    },
   },
   watch: {
     editMode() {
+      this.$active && this.getData()
+    },
+    widescreen: {
+      handler(newValue) {
+        this.totalVisible = newValue ? 9 : 5
+      },
+      immediate: true,
+    },
+    $route() {
       this.$active && this.getData()
     },
   },
