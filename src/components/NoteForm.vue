ba1d405dc1ede28a6f8a6d5d05b675a109a325f1
largezhou <largezhou@hotmail.com>
2019-08-07 09:01:48 +0800

    onSubmit 的第一个参数被默认传入 event 导致更新内容不会跳转页面的 bug

--- src/components/NoteForm.vue
+++ src/components/NoteForm.vue
@@ -53,7 +53,7 @@
       </v-container>
     </v-card-text>
     <v-card-actions>
-      <v-btn depressed color="primary" @click="onSubmit" :loading="submitting">{{ this.note ? '更新' : '添加笔记' }}</v-btn>
+      <v-btn depressed color="primary" @click="() => onSubmit()" :loading="submitting">{{ this.note ? '更新' : '添加笔记' }}</v-btn>
       <v-btn depressed @click="onReset" v-if="this.note">重置</v-btn>
       <v-btn depressed @click="onClear" v-else>清空</v-btn>
       <v-checkbox v-model="stay" v-if="this.note" label="留在此页" hide-details class="mt-0 ml-4"/>
